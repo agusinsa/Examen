9cf4de8b075f23e2e0e9881543d333cc9dd53529
agusinsa <64271811+agusinsa@users.noreply.github.com>
2021-04-14 14:14:26 -0300

    Update examen.php

--- examen/examen.php
+++ examen/examen.php
@@ -18,3 +18,14 @@ $item->datos= array(
 echo json_encode($item);
 
 ?>
+
+
+//DOCUMENTACION
+
+CREAMOS UNA CLASE Y LA DEFINIMOS COMO ITEM
+
+EL CUAL TIENE ATRIBUTOS SELLER_ID(ID DE VENDEDOR) SITE_ID(MLA)
+LUEGO A PARTIR DE ESTOS ATRIBUTOS RECORREMOS UN VECTOR CON LOS ITEM QUE TIENE ESTE VENDEDOR EN EL SITE MLA
+
+EL CUAL NOS DA COMO OUTPUT
+{"seller_id":"179571326","site_id":"MLA","datos":{"items":{"id":"id item","title":"title item","categoty_id":"categoria item","name":"name item"}}}
